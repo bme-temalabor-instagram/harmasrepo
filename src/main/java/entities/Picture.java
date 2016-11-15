7a9008d6fdb78142349bc49817328ec57a015c9c
DDoSinho <daniel.kovacs314@gmail.com>
2016-11-15 16:08:47 +0100

    Picture entitás hozzáadása

---
+++ src/main/java/entities/Picture.java
@@ -0,0 +1,37 @@
+package entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+@Entity
+public class Picture {
+
+    @Id
+    private String ID;
+
+    private String name;
+
+    public Picture() {}
+
+    public Picture(String ID, String name) {
+        this.ID = ID;
+        this.name = name;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
